85b5d1a7d041cb51eb7d57e3ab60fdee02b7db09
Alexander Protopopov <42470635+Sashko30208@users.noreply.github.com>
2018-12-01 23:30:30 +0700

    uploaded a "select server and client"

---
+++ tcpClientStealed.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <net.inet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define PORT 2442
+#define ADDR 127.0.0.1
+int main()
+{
+    int sockfd;
+    int len;
+    struct sockaddr_in address;
+    int result;
+    char ch = 'A';
+    //create socket
+    sockfd =socket(AF_INET,Sock_STREAM, 0);
+    address.sin_family=AF_INET;
+    address.sin_addr.s_addr=inet_addr(ADDR);
+    address.sin_port=htons(PORT);
+    len = sizeof(address);
+    
+    result=connect(sockfd,(struct sockaddr *)&address, len);
+    if(result==-1){
+        perror("oops: tcpClientStealed got error in connect function");
+        exit(1);
+    }
+    write(sockfd, &ch,1);
+    read(sockfd,&ch,1);
+    printf("char from server = %c\n",ch);
+    close(0);
+}
---
+++ tspselserv.c
@@ -0,0 +1,89 @@
+#include <sys/types.s>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet./in.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define PORT 2442
+int main(int argc, char const *argv[])
+{
+    int server_sockfd,client_sockfd;
+    int server_len,client_len;
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
+    int result;
+    fd_set readfds, testfds;
+    
+    /*creating a socket for server and naming it*/
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);//sock_stream->tcp
+
+    server_address.sin_family=AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(PORT);
+    server_len = sizeof(server_address);
+    
+    bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
+
+    //making a queue of query for connect
+    listen(server_sockfd,5);
+
+    //нахрен инглиш
+    //инициализирую "readfds" для обработки ввода с сокета "server_sockfd"
+    FD_ZERO(&readfds);
+    FD_SET(server_sockfd, &readfds);
+
+    while(1){
+        char ch;
+        int fd;
+        int nread;
+
+        testfds = readfds;
+
+        printf("server waiting\n");
+        result = select(FD_SETSIZE,&testfds,(fd_set *)0,(sd_set *) 0, (struct tumeval *) 0);
+        if(result<1){
+            perror("selectServ");
+            exit(1);
+        }
+
+        //проверяем какой из дискриптеров активен
+        for(fd=0;fd<FD_SETSIZE;fd++){
+            if(FD_ISSET(fd,&testfds)){
+                /*если активность на "server_sockfd",
+                 то это может быть запрос на новое соединение */
+                if(fd==server_sockfd){
+                    client_len=sizeof(client_address);
+                    client_sockfd=accept(server_sockfd, (struct sockaddr *)&client_address,&client_len);
+                    FD_SET(client_sockfd, &readfds);
+                    printf("adding client on fd %d\n", client_sockfd);
+                }
+                /*
+                если активен не сервер, то это клиент
+                */
+               else {
+                   ioctl(fd,FIONREAD, &nread);
+                   /*
+            in <sys/ioctl.h>
+            #define	FIONREAD	_IOR('f', 127, int)	/ get # bytes to read 
+            */
+                   if(nread == 0){
+                       close(fd);
+                       FD_CLR(fd,&readfds);
+                       printf("removing client on %d\n",fd);
+                   }
+                   else{
+                       read(fd,&ch,1);
+                       sleep(5);
+                       printf("serving client on fd %d\n",fd);
+                       ch++;
+                       write(fd,&ch,1);
+                   }
+
+               }
+            }
+        }
+    }
+    return 0;
+}
